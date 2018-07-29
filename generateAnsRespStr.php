f33397906eceda39982ec474350b9693f981332b
NIshat KHan <khan.nishat67@gmail.com>
2018-07-30 00:05:43 +0530

    Fixed a minor bug

--- generateAnsRespStr.php
+++ generateAnsRespStr.php
@@ -1,14 +1,15 @@
 <?php
     include_once 'db_connect.inc.php';
 
-    $sql="SELECT option1,option2,option3,option4,ans from questions";
+    $sql="SELECT opt1,opt2,opt3,opt4,ans from questions_master;";
     $result=mysqli_query($conn,$sql);
     $respStr="";
     while($row=mysqli_fetch_row($result)){
-        for($i=0;i<3;$i=$i+1){
-            if($row[$i]==$row[4])
-                $respStr=$respStr+$i;
+        for($i=0;$i<=3;$i=$i+1){
+            if($row[$i]===$row[4])
+                $respStr=$respStr . ''. $i;
         }
     }
-    echo $respStr;
+    echo $respStr.'\n';
+    echo strlen($respStr);
     ?>
